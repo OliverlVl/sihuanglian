9816305dbdc1c31cda0299e97b1847567d42cbaf
Dcat <31926067+liulqing@users.noreply.github.com>
2020-03-15 20:40:58 +0800

    web1

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+/* eslint-disable */
 Vue.use(Router)
+const baseList = resolve => require(['../components/baseList.vue'], resolve)
+const table = resolve => require(['../components/BaseTable.vue'], resolve)
+const form = resolve => require(['../components/BaseForm.vue'], resolve)
+
 
 export default new Router({
+  mode: 'history',  //把Router的mode修改为history模式,VueRouter默认的模式为HASH模式
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/baseList',
+      name: 'baseList',
+      component: baseList
+    },
+    {
+      path: '/form',
+      component: form,
+      name: 'form',
+
+    },
+    {
+      path: '/table',
+      component: table,
+      name: 'table',
+
     }
   ]
 })
