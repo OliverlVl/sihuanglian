7736e5a1566ce11c1b86f1181c730ecef70fc00a
annpion <annpion@163.com>
2020-03-27 21:00:29 +0800

    404异常页面

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -9,6 +9,8 @@ const table = resolve => require(['../components/page/BaseTable.vue'], resolve)
 const login = resolve => require(['../components/page/login.vue'], resolve)
 const forget = resolve => require(['../components/page/ForgetForm.vue'], resolve)
 const err403 = resolve => require(['../components/page/403.vue'], resolve)
+const err404 = resolve => require(['../components/page/404.vue'], resolve)
+const err500 = resolve => require(['../components/page/500.vue'], resolve)
 
 export default new Router({
   mode: 'history',  //把Router的mode修改为history模式,VueRouter默认的模式为HASH模式
@@ -61,6 +63,24 @@ export default new Router({
       name: 'err403',
       meta: {
         title: '403',
+        requireAuth: true
+      }
+    },
+    {
+      path: '/err404',
+      component: err404,
+      name: 'err404',
+      meta: {
+        title: '404',
+        requireAuth: true
+      }
+    },
+    {
+      path: '/err500',
+      component: err500,
+      name: 'err500',
+      meta: {
+        title: '500',
         requireAuth: true
       }
     }
