804307ebd75e32a9edc7c1dde8ca53c298f51fc2
lh-jiezhou <897587636@qq.com>
2020-04-02 18:49:14 +0800

    add classDAO code

--- present-cloud-server/app/dao/classDAO.js
+++ present-cloud-server/app/dao/classDAO.js
@@ -4,6 +4,45 @@ var classSqlMap = require('./classSqlMap');
 var pool = mysql.createPool(mysqlConf.mysql);
 
 module.exports = {
+     // 增insert
+     insert: function (class1, callback) {
+        pool.query(classSqlMap.insert, 
+            [class1.class_name, class1.class_life_time, class1.create_time, class1.creator, class1.update_time, class1.updater], 
+            function (error, result) {
+                if (error) throw error;
+                callback(result);
+        })
+    },
+
+    // 删delete
+    deleteById: function (class1, callback) {
+        pool.query(classSqlMap.deleteById, 
+            class1.class_id, 
+            function (error, result) {
+                if (error) throw error;
+                callback(result);
+        })
+    },
+
+    // 改update 修改班级信息
+    updateById: function (class1, callback) {
+        pool.query(classSqlMap.updateById, 
+            [class1.class_name, class1.class_life_time, class1.updater, class1.class_id],
+            function (error, result) {
+                if (error) throw error;
+                callback(result);
+        })
+    },
+
+    // 查select 查看所有
+    selectAll: function(class1, callback) {
+        pool.query(classSqlMap.selectAll, 
+            function (error, result) {
+                if (error) throw error;
+                callback(result);
+            })
+    }
+
 
 
 };
--- present-cloud-server/app/dao/classSqlMap.js
+++ present-cloud-server/app/dao/classSqlMap.js
@@ -1,13 +1,13 @@
 var classSqlMap = {
 
-    // 增
-    insert:'insert into class() values ()',
-    // 删
-    delete:'delete from class where ',
-    // 改 
-    update:'update class set  where ',
-    // 查
-    select:'select * from class where '
+      // 增 增加班级
+      insert:'insert into class(class_name, class_life_time, create_time, creator, update_time, updater) values (?, ?, ?, ?, ?, ?)',
+      // 删 通过id删除班级
+      deleteById:'delete from class where class_id=?',
+      // 改 修改班级信息
+      updateById:'update class set class_name=?, class_life_time=?, update_time=now(), updater=? where class_id=? ',
+      // 查 通过id,查看所有班级
+      selectAll:'select * from class'
 
 
 };
