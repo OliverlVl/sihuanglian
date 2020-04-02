ddfd22ed4e42750145e41b6f222d7e7ea97a841a
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2020-04-02 17:28:32 +0800

    mysql

--- present-cloud-server/app/dao/loginDAO.js
+++ present-cloud-server/app/dao/loginDAO.js
@@ -4,6 +4,38 @@ var loginiSqlMap = require('./loginiSqlMap');
 var pool = mysql.createPool(mysqlConf.mysql);
 
 module.exports = {
+    // 增insert
+    insert: function (login, callback) {
+        pool.query(loginSqlMap.insert, [login.login_name, login.login_password, login.login_type, login.user_id,], function (error, result) {
+            if (error) throw error;
+            callback(result);
+        })
+    },
+
+    // 删delete
+    deleteById: function (login, callback) {
+        pool.query(loginSqlMap.deleteById, login.login_id, function (error, result) {
+            if (error) throw error;
+            callback(result.affectedRows > 0);
+        })
+    },
+
+    // 改update
+    updateById: function (login, callback) {
+        pool.query(loginSqlMap.updateById, [login.login_name, login.password, login.login_id], function (error, result) {
+            if (error) throw error;
+            callback(result.affectedRows > 0);
+        })
+    },
+
+    // 查select
+    selectByUserId: function (login, callback) {
+        pool.query(loginSqlMap.selectByCourseId, login.user_id, function (error, result) {
+            if (error) throw error;
+            callback(result);
+        })
+    },
+
 
 
 };
--- present-cloud-server/app/dao/loginSqlMap.js
+++ present-cloud-server/app/dao/loginSqlMap.js
@@ -1,13 +1,13 @@
 var loginSqlMap = {
 
     // 增
-    insert:'insert into login() values ()',
+    insert:'insert into login(login_name, login_password, login_type, user_id) values (?, ?, ?, ?)',
     // 删
-    delete:'delete from login where ',
+    delete:'delete from login where login_id = ?',
     // 改 
-    update:'update login set  where ',
+    update:'update login set login_name = ? , login_password = ? where login_id = ?',
     // 查
-    select:'select * from login where '
+    select:'select * from login where user_id = ?'
 
 
 };
