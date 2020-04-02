42ac043d083a7436605ce5c3365ac6469ceeb3e5
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2020-04-02 17:20:28 +0800

    mysql

--- present-cloud-server/app/dao/scoreSqlMap.js
+++ present-cloud-server/app/dao/scoreSqlMap.js
@@ -1,13 +1,13 @@
 var scoreSqlMap = {
 
     // 增
-    insert:'insert into score() values ()',
+    insert:'insert into score(student_id, course_id) values (?, ?)',
     // 删
-    delete:'delete from score where ',
+    delete:'delete from score where score_id = ?',
     // 改 
-    update:'update score set  where ',
+    update:'update score set usual_score = ?, exam_score = ?, final_score = ? where score_id = ?',
     // 查
-    select:'select * from score where '
+    select:'select * from score where student_id = ? and course_id = ?'
 
 
 };
