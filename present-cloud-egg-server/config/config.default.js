fc649f49818e213c1efe7f3d2987f08038625f37
lh-jiezhou <897587636@qq.com>
2020-04-17 21:54:48 +0800

    配置数据库连接池

--- present-cloud-egg-server/config/config.default.js
+++ present-cloud-egg-server/config/config.default.js
@@ -26,6 +26,11 @@ module.exports = appInfo => {
     database: 'present-cloud', // 数据库名
     username: 'root', // 数据库用户名
     password: 'Admin888', // 数据库密码
+    pool: {
+      max: 5, // 连接池中最大连接数量
+      min: 0, // 连接池中最小连接数量
+      idle: 10000 // 如果一个线程 10 秒钟内没有被使用过的话，那么就释放线程
+    },
     define: { // model的全局配置
       //freezeTableName默认值是 false 如果是false的话，会自动在表名后加s复数
       freezeTableName: true, // 防止修改表名为复数
