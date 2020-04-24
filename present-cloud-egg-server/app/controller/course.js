05e3e19317189de9b9a316fe50ed84543c5ede32
lh-jiezhou <897587636@qq.com>
2020-04-24 16:19:00 +0800

    学生通过班课编号查找班课

--- present-cloud-egg-server/app/controller/course.js
+++ present-cloud-egg-server/app/controller/course.js
@@ -7,6 +7,18 @@ class CourseController extends Controller {
 		const { ctx } = this;
 		ctx.body = 'hi, egg';
 	}
+
+	// 学生通过班课编号查找班课
+	async selectCourse() {
+		const { ctx } = this
+		const num = ctx.query
+		console.log(num.course_number)
+		const result = await ctx.service.course.selectCourse(num.course_number);
+		console.log(result)
+		ctx.body = result
+	}
+
+
 }
 
 module.exports = CourseController;
