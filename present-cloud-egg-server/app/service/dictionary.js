fe5e521fefc2f81f1a2551fcda97eafc52d071a8
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2020-04-24 17:57:40 +0800

    初始化

---
+++ present-cloud-egg-server/app/service/dictionary.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class DictionaryService extends Service {
+	async index() {
+
+	}
+}
+
+module.exports = DictionaryService;
---
+++ present-cloud-egg-server/app/service/dictionary_detail.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Service = require('egg').Service;
+
+class DictionaryDetailService extends Service {
+	async index() {
+
+	}
+}
+
+module.exports = DictionaryDetailService;
