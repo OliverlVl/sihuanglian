3534594f1f29597e350760d86e1b41cadea9ae4f
lh-jiezhou <897587636@qq.com>
2020-04-24 18:53:25 +0800

    查询字典项，修改字典项

--- present-cloud-egg-server/app/service/dictionary_detail.js
+++ present-cloud-egg-server/app/service/dictionary_detail.js
@@ -3,9 +3,53 @@
 const Service = require('egg').Service;
 
 class DictionaryDetailService extends Service {
-	async index() {
-
+	// 查询字典 详情
+	async selectDictionaryDetail(code) {
+		const { ctx } = this
+		const result = await ctx.model.DictionaryDetail.findAll({
+			where: {
+				code: code
+			}
+		})
+		console.log(result)
+		if (result != 0) {
+			return result
+		} else {
+			return {
+				code: -1,
+				msg: "为空"
+			}
+		}
 	}
+
+	// 根据id修改item_value
+	async updateDictionaryDetail(detail_id, item_value) {
+		const { ctx } = this
+		// result 返回数据条数
+		const result = await ctx.model.DictionaryDetail.update(
+			{
+				item_value: item_value
+			},
+			{
+				where: {
+					detail_id: detail_id
+				}
+
+			})
+		// console.log(result[0])
+		if(result[0] != 0){
+			return {
+				code: 200,
+				msg: "修改成功"
+			}
+		} else{
+			return {
+				code: -1,
+				msg: "修改失败"
+			}
+		}
+	}
+
 }
 
 module.exports = DictionaryDetailService;
