3addd913567a573be9af595e9710ad9c06362075
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2020-06-19 20:34:47 +0800

    Update hello.js

--- present-cloud-egg-server/app/controller/hello.js
+++ present-cloud-egg-server/app/controller/hello.js
@@ -14,6 +14,23 @@ class helloController extends Controller {
         const loginMsg = ctx.request.body; // request.body获取前端post值
         const result = await ctx.service.login.login(loginMsg);
         ctx.body = result;
+	}
+	
+	 // 注册方法
+	 async register() {
+        const { ctx } = this;
+        const registerMsg = ctx.request.body;
+        const result = await ctx.service.login.register(registerMsg);
+        ctx.body = result;
+	}
+	
+    //修改密码
+    async updatePassword() {
+        const { ctx } = this;
+		const passwordMsg = ctx.request.body;
+		const result = await ctx.service.login.updatePassword(passwordMsg);
+        ctx.body = result;
+
     }
 
 
