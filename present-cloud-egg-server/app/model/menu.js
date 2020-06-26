18816ac98ab8c74a05a848a0fcee182182a22161
lh-jiezhou <897587636@qq.com>
2020-06-26 21:26:57 +0800

    添加菜单model

---
+++ present-cloud-egg-server/app/model/menu.js
@@ -0,0 +1,35 @@
+'use strict'
+/* jshint indent: 1 */
+
+module.exports = app => {
+    const { STRING, INTEGER } = app.Sequelize; //获取 sequelize对应的数据类型映射
+
+	const Menu = app.model.define('menu', {
+		id: {
+			type: INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		name: {
+			type: STRING,
+			allowNull: false
+		},
+		state: {
+			type: INTEGER,
+			allowNull: false
+		},
+		layer: {
+			type: INTEGER,
+			allowNull: true
+		},
+		sub: {
+			type: STRING,
+			allowNull: true
+		}
+	}, {
+		tableName: 'menu'
+	});
+
+	return Menu;
+};
