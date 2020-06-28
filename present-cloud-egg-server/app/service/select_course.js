d574db9277e82f6e7f7c6c133c71937f129d9384
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2020-06-28 23:42:56 +0800

    Update select_course.js

--- present-cloud-egg-server/app/service/select_course.js
+++ present-cloud-egg-server/app/service/select_course.js
@@ -3,33 +3,75 @@
 const Service = require('egg').Service;
 
 class SelectCourseService extends Service {
+
+	// 获取十条班课信息
+	async selectCourInfoByPage(page) {
+		const { ctx } = this
+		var result = {}
+		for (var i = 0; i < 10; i++) {
+
+			const container = await ctx.model.SelectCourse.findOne({
+				where: {
+					select_course_id: page
+				}
+			})
+			if (container != null) {
+				const student = await ctx.model.Student.findOne({
+					where: {
+						student_id: container.student_id
+					}
+				})
+				// 添加学号和姓名
+				container.dataValues['student_number'] = student.student_number
+				container.dataValues['student_name'] = student.student_name
+				const course = await ctx.model.Course.findOne({
+					where: {
+						course_id: container.course_id
+					}
+				})
+				// 添加课程名
+				container.dataValues['course_name'] = course.course_name
+
+				// console.log(container.dataValues)
+				result[i + 1] = container.dataValues
+				page++;
+			}
+
+		}
+		// console.log(result)
+
+		return result;
+	}
+
 	// 学生加入班课
-	async addCourse(student_id, course_id){
+	async addCourse(student_id, course_id) {
 		const { ctx } = this;
 		const select_course = new ctx.model.SelectCourse
 		select_course.student_id = student_id
 		select_course.course_id = course_id
 		// 默认值
 		select_course.course_experience_point = 0
-		
+
 		// console.log(select_course)
 		const result = await ctx.model.SelectCourse.create(select_course.dataValues)
 		// console.log(result==null)
-		if(result == null ){
+		if (result == null) {
 			// 创建失败
 			return {
 				code: -1,
 				msg: "加入失败"
 			}
-		} else{
+		} else {
 			return {
 				code: 200,
 				msg: "加入成功"
 			}
 		}
-		
+
 	}
-	
+
+
+
 }
 
 module.exports = SelectCourseService;
