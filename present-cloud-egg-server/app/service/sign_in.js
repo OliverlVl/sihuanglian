bb8e7decbb2345b3c88303dba4e61907e8570448
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2020-07-01 22:54:32 +0800

    Update sign_in.js

--- present-cloud-egg-server/app/service/sign_in.js
+++ present-cloud-egg-server/app/service/sign_in.js
@@ -20,17 +20,6 @@ class SignInService extends Service {
 	}
 
 
-
-
-	// 教师签到详情
-	/**
-	 * 
-	 * @param {*} courseId 
-	 */
-	async teachrerSignInInfo(courseId){
-
-	}
-
 }
 
 module.exports = SignInService;
