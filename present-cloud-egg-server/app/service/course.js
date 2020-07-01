c0f5af31a2d642aa266bb742039d78f6b40d15fb
OliverlVl <54763262+OliverlVl@users.noreply.github.com>
2020-07-01 16:26:59 +0800

    Update course.js

--- present-cloud-egg-server/app/service/course.js
+++ present-cloud-egg-server/app/service/course.js
@@ -4,22 +4,44 @@ const Service = require('egg').Service;
 
 class CourseService extends Service {
 
-	// 学生通过班课编号查找班课
-	async selectCourse(course_number) {
+    // 根据课程id获取课程信息
+    async selcetCourseInfoByCourseId(courseId) {
         const { ctx } = this
         const result = await ctx.model.Course.findAll({
-            where: { 
-                course_number: course_number,
-            }, 
-		})
-		// console.log(result)
-        if(result != 0){
-            return  result
-        } else{
+            where: {
+                course_id: courseId,
+            },
+        })
+        if (result != 0) {
+            return result
+        } else {
             return {
-				code: -1,
-				msg: "班课不存在"
-			}
+                code: -1,
+                msg: "班课不存在"
+            }
+        }
+
+    }
+
+
+
+
+    // 学生通过班课编号查找班课
+    async selectCourse(course_number) {
+        const { ctx } = this
+        const result = await ctx.model.Course.findAll({
+            where: {
+                course_number: course_number,
+            },
+        })
+        // console.log(result)
+        if (result != 0) {
+            return result
+        } else {
+            return {
+                code: -1,
+                msg: "班课不存在"
+            }
         }
     }
 
