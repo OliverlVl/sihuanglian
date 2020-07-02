0bc310f6b1c981192fe85b18ea32019acdff4d7f
lh-jiezhou <897587636@qq.com>
2020-07-02 22:54:52 +0800

    添加时间信息

--- present-cloud-egg-server/app/service/feedback.js
+++ present-cloud-egg-server/app/service/feedback.js
@@ -7,6 +7,38 @@ class FeedbackService extends Service {
 
 	}
 
+	// 获取当前时间
+	getNowFormatDate  () {
+        var date = new Date();
+        var seperator1 = "-";
+        var seperator2 = ":";
+        var month = date.getMonth() + 1;
+        var strDate = date.getDate();
+        if (month >= 1 && month <= 9) {
+            month = "0" + month;
+        }
+        if (strDate >= 0 && strDate <= 9) {
+            strDate = "0" + strDate;
+        }
+        var hours = date.getHours();
+        if(hours >=0 && hours <=9){
+            hours = "0" + hours;
+        }
+        var minutes = date.getMinutes();
+        if(minutes >=0 && minutes <=9){
+            minutes = "0" + minutes;
+        }
+        var seconds = date.getSeconds();
+        if(seconds >=0 && seconds <=9){
+            seconds = "0" + seconds;
+        }
+        var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+                + " " + hours + seperator2 + minutes
+                + seperator2 + seconds;
+        return currentdate;
+	}
+
+
 	// 添加反馈信息
 	async addFeedback(feedbackMsg) {
 		const { ctx } = this;
@@ -16,8 +48,13 @@ class FeedbackService extends Service {
 		const result = await ctx.model.Feedback.create({
 			user_id: userId,
 			role: role,
-			content: feedbackMsg.content 
+			content: feedbackMsg.content,
+			create_time: this.getNowFormatDate(),
+			creator: userId,
+			update_time: this.getNowFormatDate(),
+			updater: userId
 		})
+		// console.log(this.getNowFormatDate())
 		if (result == null) {
 			return false // 错误
 		}
