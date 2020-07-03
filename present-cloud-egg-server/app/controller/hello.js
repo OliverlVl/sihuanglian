f0dcb9903f3da7f0c76dd49d3ca461ec1a49faaf
lh-jiezhou <897587636@qq.com>
2020-07-03 23:24:19 +0800

    controller登录方法修改

--- present-cloud-egg-server/app/controller/hello.js
+++ present-cloud-egg-server/app/controller/hello.js
@@ -13,7 +13,7 @@ class helloController extends Controller {
 	// 登录方法
 	async login() {
 		const { ctx } = this;
-		const loginMsg = ctx.query; // request.body获取前端post值
+		const loginMsg = ctx.request.body; // request.body获取前端post值
 		const result = await ctx.service.login.login(loginMsg);
 		ctx.body = result;
 	}
